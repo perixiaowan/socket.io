9ea32da80c4c02f5c9b09d2b16bf21725f0af266
perixiaowan <xiaowanxxww@163.com>
2017-08-26 09:32:05 +0800

    fix meta bugs 02

--- examples/chat/public/main.js
+++ examples/chat/public/main.js
@@ -6,13 +6,13 @@ $(function() {
   var metaEl = document.querySelector('meta[name="viewport"]');
 
   dpr = window.devicePixelRatio || 1;
-  rem = docEl.clientWidth * dpr / 10;
+  rem = docEl.clientWidth * dpr / 12;
   scale = 1 / dpr;
   dpr_docEL = dpr * docEl.clientWidth;
 
 
   // 设置viewport，进行缩放，达到高清效果
-  metaEl.setAttribute('content', 'width=' + dpr_docEL + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
+  //metaEl.setAttribute('content', 'width=' + dpr_docEL + ',initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale + ',user-scalable=no');
 
   // 设置data-dpr属性，留作的css hack之用
   docEl.setAttribute('data-dpr', dpr);
